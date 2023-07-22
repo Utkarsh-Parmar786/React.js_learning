10c246583fe3585e801d9a88941f028bceeafcff
Utkarsh Parmar <utkarshparmar786420@gmail.com>
2023-07-23 03:31:02 +0530

    usage of map

--- day-1/practiceapp/src/App.js
+++ day-1/practiceapp/src/App.js
@@ -1,24 +1,22 @@
 import "./App.css";
 import Card from './Card';
+import Hdata from "./Hdata";
 
 function App() {
   return (
     <>
-      <Card imgSrc="https://pbs.twimg.com/media/E5wCyt5VIAIM_Eg.jpg:large" 
-      sname="Disney Plus Hotstar"
-      title = "Bhoot Police" 
-      link = "https://www.hotstar.com/in/movies/bhoot-police/1260067821"/>
 
-      <Card imgSrc="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_2_5x/sources/r1/cms/prod/5576/675576-v"
-      sname="Disney Plus Hotstar"
-      title="Aladdin"
-      link="https://www.hotstar.com/in/movies/aladdin/1260014815" />
-
-      <Card imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq-gV5vApcQyCRKtamXenVl4flCF1N27By3Q&usqp=CAU"
-      sname="Disney Plus Hotstar"
-      title="IB71"
-      link="https://www.hotstar.com/in/movies/ib71/1260143875?filters=content_type%3Dmovie" />
-
+      <h1 className="heading_style">List of Movies </h1>
+ 
+     
+      {Hdata.map((val)=>{
+        return(
+        <Card imgSrc={val.imgSrc} 
+              sname={val.sname}
+              title = {val.title} 
+              link = {val.link} />
+            )
+      })}
     </>
   );
 }
---
+++ day-1/practiceapp/src/Hdata.jsx
@@ -0,0 +1,22 @@
+const Hdata = [
+    {
+        imgSrc:"https://pbs.twimg.com/media/E5wCyt5VIAIM_Eg.jpg:large", 
+        sname:"Disney Plus Hotstar",
+        title:"Bhoot Police",
+        link:"https://www.hotstar.com/in/movies/bhoot-police/1260067821"
+    },
+    {
+        imgSrc:"https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_2_5x/sources/r1/cms/prod/5576/675576-v",
+        sname:"Disney Plus Hotstar",
+        title:"Aladdin",
+        link:"https://www.hotstar.com/in/movies/aladdin/1260014815"
+    },
+    {
+       imgSrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq-gV5vApcQyCRKtamXenVl4flCF1N27By3Q&usqp=CAU",
+       sname:"Disney Plus Hotstar",
+       title:"IB71",
+       link:"https://www.hotstar.com/in/movies/ib71/1260143875?filters=content_type%3Dmovie"
+    }
+]
+
+export default Hdata;
